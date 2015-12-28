520683af7b03ea2913df13ab76da66912371c6a1
Raphael Matile <raphael.matile@gmail.com>
2015-12-28 16:06:16 +0100

    Add utility for conflict file naming

--- src/main/java/org/rmatil/sync/commons/path/Naming.java
+++ src/main/java/org/rmatil/sync/commons/path/Naming.java
@@ -1,12 +1,13 @@
 package org.rmatil.sync.commons.path;
 
-public class Naming {
+import java.nio.file.Paths;
 
+public class Naming {
 
     /**
      * Returns the path to the file without the filename
      *
-     * @param fileName The filename without the path
+     * @param fileName               The filename without the path
      * @param pathToFileWithFilename The path containing also the filename
      *
      * @return The path to the file without the filename and without an ending slash
@@ -17,6 +18,43 @@ public class Naming {
         int idx = pathToFileWithFilename.indexOf(fileName);
         // remove slash between path and filename
         return pathToFileWithFilename.substring(0, idx - 1);
+    }
+
+    /**
+     * Assembles the file name for conflict files
+     *
+     * @param relativeFilePath The relative path to the file (incl. the filename)
+     * @param isFile           Whether the path represents a file (i.e. a dot is present in the filename)
+     * @param fileExtension    The file extension of the file
+     * @param clientDeviceId   The id of the client which is added to the filename
+     *
+     * @return The file name of the conflict file
+     *
+     * @throws IllegalArgumentException If the clientDeviceId contains illegal characters (dot)
+     */
+    public static String getConflictFileName(String relativeFilePath, boolean isFile, String fileExtension, String clientDeviceId)
+            throws IllegalArgumentException {
+
+        if (clientDeviceId.contains(".")) {
+            throw new IllegalArgumentException("The device id must not have any dots in it");
+        }
+
+        String lastPathName = Paths.get(relativeFilePath).getFileName().toString();
+
+        String newFileName = lastPathName;
+        if (isFile && fileExtension.length() > 0) {
+            int extStartIndex = lastPathName.lastIndexOf(fileExtension);
+
+            // decrease the position of the dot
+            extStartIndex = (extStartIndex > 0) ? extStartIndex - 1 : extStartIndex;
+
+            newFileName = lastPathName.substring(0, extStartIndex);
+        }
+
+        // add client name
+        newFileName = newFileName + "_" + clientDeviceId;
+        // re-add file extension
+        return newFileName + ((fileExtension.length() > 0) ? ("." + fileExtension) : "");
     }
 
 }
--- src/test/java/org/rmatil/sync/commons/test/naming/NamingTest.java
+++ src/test/java/org/rmatil/sync/commons/test/naming/NamingTest.java
@@ -1,11 +1,16 @@
 package org.rmatil.sync.commons.test.naming;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.rmatil.sync.commons.path.Naming;
 
 import static org.junit.Assert.*;
 
 public class NamingTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testGetPathWithoutFileName() {
@@ -19,5 +24,35 @@ public class NamingTest {
 
         String f3 = "/my/Path/To/File.txt";
         assertEquals("Path is not correct", "/my/Path/To", Naming.getPathWithoutFileName(fn1, f3));
+    }
+
+    @Test
+    public void testGetConflictFileName() {
+        String deviceId = "client1";
+        String illegalDeviceId = "client.1";
+
+        String fn1 = "myPath/to/myFile.txt";
+        String fn2 = "myPath/to/myFile.tar.gz";
+        String fn3 = "myPath/to/myFileWithoutExtension";
+        String fn4 = "myPath/to/myDir";
+        String fn5 = "myPath/to/myDir.With.Dots";
+
+        String ret1 = Naming.getConflictFileName(fn1, true, "txt", deviceId);
+        assertEquals("Filename for fn1 is not equal", "myFile_client1.txt", ret1);
+
+        String ret2 = Naming.getConflictFileName(fn2, true, "tar.gz", deviceId);
+        assertEquals("Filename for fn2 is not equal", "myFile_client1.tar.gz", ret2);
+
+        String ret3 = Naming.getConflictFileName(fn3, true, "", deviceId);
+        assertEquals("Filename for fn3 is not equal", "myFileWithoutExtension_client1", ret3);
+
+        String ret4 = Naming.getConflictFileName(fn4, false, "", deviceId);
+        assertEquals("Filename for fn4 is not equal", "myDir_client1", ret4);
+
+        String ret5 = Naming.getConflictFileName(fn5, false, "", deviceId);
+        assertEquals("Filename for fn4 is not equal", "myDir.With.Dots_client1", ret5);
+
+        thrown.expect(IllegalArgumentException.class);
+        Naming.getConflictFileName(fn1, true, "txt", illegalDeviceId);
     }
 }
